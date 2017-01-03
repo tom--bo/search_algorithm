f9dfe46f3648f063a2b6c10a2ae62826f22c30f9
tom--bo <otsuka.t.2013@gmail.com>
2017-01-03 11:35:39 +0900

    Make tests tidy

--- search_test.go
+++ search_test.go
@@ -1,37 +1,31 @@
 package main
 
 import (
-	_ "fmt"
 	"reflect"
 	"testing"
 )
 
 func TestSearch(t *testing.T) {
-	if !reflect.DeepEqual(bm("abcde", "ab"), []int{0}) {
-		t.Fatal("Failed at case1")
+	var tests = []struct {
+		text, query string
+		want        []int
+	}{
+		{"abcde", "ab", []int{0}},
+		{"aaaaa", "aa", []int{0, 1, 2, 3}},
+		{"abcde\nabcd", "ab", []int{0, 6}},
+		{"abcde", "ac", []int{}},
+		{"abcde", "a", []int{0}},
+		{"abcde", "", []int{}},
+		{"which finally halts.  at that point", "at that", []int{22}},
 	}
 
-	if !reflect.DeepEqual(bm("aaaaa", "aa"), []int{0, 1, 2, 3}) {
-		t.Fatal("Failed at case2")
+	for num, tc := range tests {
+		got := bm(tc.text, tc.query)
+		want := tc.want
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got -> %v, want -> %v\n", got, want)
+			t.Fatal("Failed at case:", num)
+		}
 	}
 
-	if !reflect.DeepEqual(bm("abcdeab", "ab"), []int{0, 5}) {
-		t.Fatal("Failed at case3")
-	}
-
-	if !reflect.DeepEqual(bm("abcde\nabcd", "ab"), []int{0, 6}) {
-		t.Fatal("Failed at case4")
-	}
-
-	if !reflect.DeepEqual(bm("abcde", "ac"), []int{}) {
-		t.Fatal("Failed at case5")
-	}
-
-	if !reflect.DeepEqual(bm("abcde", "a"), []int{0}) {
-		t.Fatal("Failed at case6")
-	}
-
-	if !reflect.DeepEqual(bm("abcde", ""), []int{}) {
-		t.Fatal("Failed at case7")
-	}
 }
