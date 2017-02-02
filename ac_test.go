780c2388f624f225439d1c47e7c1a5934d2146ad
tom--bo <otsuka.t.2013@gmail.com>
2017-02-02 15:32:25 +0900

    Add ac_test

---
+++ ac_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestACSearch(t *testing.T) {
+	var tests = []struct {
+		text  string
+		query []string
+		want  []int
+	}{
+		{"", []string{}, []int{}},
+		{"", []string{"a"}, []int{}},
+		{"abc", []string{""}, []int{}},
+		{"abcde", []string{"ab"}, []int{0}},
+		{"xbabcdex", []string{"ab", "bc", "bab", "d", "abcde"}, []int{1, 2, 3, 5}},
+	}
+
+	for num, tc := range tests {
+		got := ac(tc.text, tc.query)
+		want := tc.want
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got -> %v, want -> %v\n", got, want)
+			t.Fatal("Failed at case:", num)
+		}
+	}
+
+}
