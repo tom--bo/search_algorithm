d511efe61829dfaf75b075e32ad1a78a09ad5f2c
tom--bo <otsuka.t.2013@gmail.com>
2017-02-05 23:15:31 +0900

    Add test case to ac_test.go

--- ac_test.go
+++ ac_test.go
@@ -13,11 +13,13 @@ func TestACSearch(t *testing.T) {
 	}{
 		{"", []string{}, []int{}},
 		{"", []string{"a"}, []int{}},
-		//{"abc", []string{""}, []int{}},
+		{"abc", []string{"", ""}, []int{}},
 		{"abcde", []string{"ab"}, []int{0}},
 		{"xbabcdex", []string{"ab", "abcde"}, []int{2, 2}},
 		{"xbabcdex", []string{"x", "ab", "abcde"}, []int{0, 2, 2, 7}},
 		{"xbabcdex", []string{"ab", "bc", "bab", "d", "abcde"}, []int{1, 2, 2, 3, 5}},
+		{"aaaa", []string{"a", "b"}, []int{0, 1, 2, 3}},
+		{"which finally halts.  at that point", []string{"at that"}, []int{22}},
 	}
 
 	for num, tc := range tests {
